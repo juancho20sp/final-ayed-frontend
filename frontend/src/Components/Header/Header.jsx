8a99e4dee81bdfc086345d551478a3d79a58137f
juancho20sp <juancho20sp@gmail.com>
2020-11-19 14:10:59 -0500

    Feature 1

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="header__container">Header</div>;
 };
 
 export default Header;
