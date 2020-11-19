18facb101373d1d73fd55020fc7e3eccba73bf40
Juan David Murillo <50994778+juancho20sp@users.noreply.github.com>
2020-11-19 15:07:09 -0500

    Develop (#1)
    * Initial state
    
    * Feature 1
    
    * Router feature added

---
+++ frontend/src/Routes/Routes.js
@@ -0,0 +1,19 @@
+export default {
+  home: {
+    id: 0,
+    path: "/",
+    name: "home"
+  },
+
+  graphs: {
+    id: 1,
+    path: "/graphs",
+    name: "graphs"
+  },
+
+  heap: {
+    id: 2,
+    path: "/heap",
+    name: "heap"
+  }
+};
---
+++ frontend/src/Views/Graphs/Graphs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Graphs = () => {
+  return <div>Graph component!</div>;
+};
+
+export default Graphs;
